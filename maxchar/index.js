78e901b20d640f45c1c09217bdbd3aa1c988df0c
conor.ney <conor.ux@gmail.com>
2022-09-05 00:37:24 -0400

    max char challenge solved

---
+++ maxchar/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const chars = {};
+  for (let char of str) {
+    chars[char] ? chars[char]++ : (chars[char] = 1);
+  }
+  let max = 0;
+  let letter;
+  for (let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      letter = char;
+    }
+  }
+  return letter;
+}
+
+module.exports = maxChar;
