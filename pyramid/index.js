a7ef1865b96b386ca254ff9661b558593b85b145
conor.ney <conor.ux@gmail.com>
2022-09-08 00:47:22 -0400

    passed pyramid test iteratively

---
+++ pyramid/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+//    pyramid(4)
+//      '   #   '
+//      '  ###  '
+//      ' ##### '
+//      '#######'
+
+function pyramid(n) {
+  const counter = 1;
+  const max = n + n - 1;
+  for (let height = 0; height < n; height++) {
+    let str = '';
+    for (let length = 0; length < max; length++) {
+      if (length < n - height - 1) str += ' ';
+      if (!(length < n - height - 1) && !(length > n + height - 1)) str += '#';
+      if (length > n + height - 1) str += ' ';
+    }
+
+    console.log(str);
+  }
+}
+pyramid(4);
+module.exports = pyramid;
--- steps/index.js
+++ steps/index.js
@@ -36,35 +36,36 @@
 //   }
 // }
 
-// function steps(n) {
-//   for (let row = 0; row < n; row++) {
-//     let stair = '';
-//     for (let column = 0; column < n; column++) {
-//       if (column <= row) {
-//         stair += '#';
-//       } else {
-//         stair += ' ';
-//       }
-//     }
-//     console.log(stair);
-//   }
-// }
-
-function steps(n, row = 0, stair = '') {
-  if (n === row) {
-    return;
-  }
-  if (n === stair.length) {
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
     console.log(stair);
-    steps(n, row + 1);
   }
-  if (stair.length <= row) {
-    stair += '#';
-  } else {
-    stair += '';
-  }
-  steps(n, row, stair);
 }
 
-steps(4);
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) return;
+
+//   if (n === stair.length) {
+//     console.log(stair);
+//     steps(n, row + 1);
+//   }
+
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+//   // console.log(stair);
+//   steps(n, row, stair);
+// }
+
+steps(10);
 module.exports = steps;
