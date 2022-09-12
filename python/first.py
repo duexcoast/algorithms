0b274d0a71d52f0c2a65e74fcfb94e66e5a93d4e
conor.ney <conor.ux@gmail.com>
2022-09-12 16:15:15 -0400

    python branching

--- python/first.py
+++ python/first.py
@@ -1,3 +1,19 @@
-import random
 
-print( random.randint(16, 272))
+# a = float(input("give me a number: "))
+
+# b = float(input("give me a second number: "))
+
+# if a == b:
+#     print("those are the same numbers")
+#     if b!= 0:
+#         print("therefore, the quotient is ", a/b)
+# elif a > b:
+#     print("your first number was bigger")
+# else:
+#     print("your second number was bigger")
+
+mysum = 0
+
+for i in range(6, 10, 2):
+    mysum += i
+    print(mysum)
