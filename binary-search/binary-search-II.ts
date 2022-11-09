787e3d653066b1f6ab5dd24149131e2c1ba43cc0
conor.ney <conor.ux@gmail.com>
2022-11-09 11:06:55 -0500

    binary search II

---
+++ binary-search/binary-search-II.ts
@@ -0,0 +1,28 @@
+// This ensures that the search size will always be 2;
+// Allowing you to make comparisons on mid and it's right neighbor;
+// Useful when you need to make that comparison to decide whether
+// to go left or right when narrowing.
+
+function binarySearchII(arr: number[], target: number): number {
+  if (arr.length === 0) return -1;
+
+  let left: number = 0;
+  let right: number = arr.length;
+
+  while (left < right) {
+    let mid: number = left + (right - left) / 2;
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  // Post processing:
+  // End condition: left === right
+  if (left !== arr.length && arr[left] === target) return left;
+  return -1;
+}
